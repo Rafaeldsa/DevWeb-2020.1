da9d5da8998075fda872e0495afa602698ab06e6
rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-03-13 14:32:10 -0300

    add index

---
+++ app.js
@@ -0,0 +1,10 @@
+var div = document.getElementById("#form");
+var button = document.getElementById("#login");
+
+button.onclick = function () {
+    var newDiv = document.createElement("div");
+    newDiv.style.width = 100;
+    newDiv.style.height = 100;
+    newDiv.style.backgroundColor = "#f00";
+    div1.appendChild(newDiv);
+}
