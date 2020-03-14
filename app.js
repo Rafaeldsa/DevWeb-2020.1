17127367358f9f99797c9626c8436ab5ce3f56ed
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-03-14 10:26:24 -0300

    updates

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var button = document.querySelector("button#login");
-        var div1 = document.querySelector("div#form");
-        button.onclick = function () {
-            var newDiv = document.createElement("div");
-            newDiv.style.width = 100;
-            newDiv.style.height = 100;
-            newDiv.style.backgroundColor = "#f00";
-            div1.appendChild(newDiv);
-        }
+var button = document.querySelector('button#login')
+var div1 = document.querySelector('div#form')
+
+button.onclick = function () {
+  var newDiv = document.createElement('div')
+  newDiv.style.width = 100
+  newDiv.style.height = 100
+  newDiv.style.backgroundColor = '#f00'
+  div1.appendChild(newDiv)
+}
--- donation.js
+++ donation.js
@@ -1,24 +1,30 @@
-var div = document.querySelector("div#doacoes");
-var btn = document.querySelector("button#btn1");
-var body = document.querySelector("body#corpo");
-div.style.margin = 0;
-div.style.width = 250;
-div.style.height = 100;
-div.style.backgroundColor = "#f00";
+var div = document.querySelector('div#doacoes')
+var btn = document.querySelector('button#btn1')
+var body = document.querySelector('body#corpo')
+div.style.margin = 0
+div.style.width = 250
+div.style.height = 100
+div.style.backgroundColor = '#f00'
 
+btn.onclick = async () => {
+  let response = await fetch('data.json')
+  let dados = await response.json()
+  var doador = dados[0]['nome']
+  var tipo = dados[0]['tipo']
+  var newDiv = document.createElement('div')
 
-btn.onclick = function() {
-    var newDiv = document.createElement("div");
+  var h51 = document.createElement('h5')
+  var textNode = document.createTextNode(doador)
+  h51.appendChild(textNode)
+  var h52 = document.createElement('h5')
+  var textNode2 = document.createTextNode(tipo)
+  h52.appendChild(textNode2)
+  newDiv.appendChild(h51)
+  newDiv.appendChild(h52)
 
-    var h5 = document.createElement("h5");
-    var textNode = document.createTextNode("Doador");
-    h5.appendChild(textNode);
-    newDiv.appendChild(h5);
-
-    
-    newDiv.style.margin = 0;
-    newDiv.style.width = 250;
-    newDiv.style.height = 100;
-    newDiv.style.backgroundColor = "#f00";
-    body.appendChild(newDiv);
-};
+  newDiv.style.margin = 0
+  newDiv.style.width = 250
+  newDiv.style.height = 100
+  newDiv.style.backgroundColor = '#f00'
+  body.appendChild(newDiv)
+}
